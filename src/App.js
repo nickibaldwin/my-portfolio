a3ccc4a3a64c9942d8978b2952fc9a02f26145f7
Nicki Baldwin <nickibaldwin@gmail.com>
2021-11-17 19:44:50 -0800

    about section phase1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import About from "./components/About";
+// import Contact from "./components/Contact";
+// import Navbar from "./components/Navbar";
+// import Portfolio from "./components/Portfolio";
+// import Resume from "./components/Resume";
 
 export default function App() {
-  return "Nicki Baldwin";
+  return (
+    <main className="text-gray-900 bg-gray-200 body-font">
+      {/* <Navbar /> */}
+      <About />
+      {/* <Resume />
+      <Portfolio />
+      <Contact /> */}
+    </main>
+  )
 }
